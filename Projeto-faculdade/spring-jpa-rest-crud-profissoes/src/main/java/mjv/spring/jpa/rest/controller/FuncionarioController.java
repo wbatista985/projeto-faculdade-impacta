fadd4a5229ed512889b60b38b242d32bc74d6c43
Wagner Santos <wbatista985@gmail.com>
2025-03-09 13:41:42 -0300

    feat - Implementação  Incluir Funcionário

---
+++ Projeto-faculdade/spring-jpa-rest-crud-profissoes/src/main/java/mjv/spring/jpa/rest/controller/FuncionarioController.java
@@ -0,0 +1,35 @@
+package mjv.spring.jpa.rest.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import mjv.spring.jpa.rest.dto.FuncionarioDTO;
+import mjv.spring.jpa.rest.model.Funcionario;
+import mjv.spring.jpa.rest.service.FuncionarioService;
+
+@RestController
+@RequestMapping("/funcionarios")
+@CrossOrigin
+public class FuncionarioController {
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	@PostMapping(path = "/inserir")
+	public ResponseEntity<Funcionario> inserir(@RequestBody FuncionarioDTO funcionarioDto) {
+		Funcionario funcionario = funcionarioService.fromDTO(funcionarioDto);
+		Funcionario novoFuncionario = funcionarioService.inserirFuncionario(funcionario);
+		return ResponseEntity.ok().body(novoFuncionario);
+	}
+
+}
